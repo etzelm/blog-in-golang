1176106e6a6a8cfbccb003f74f2cba63b82d44fb
Mitchell Etzel <metzel@ucsc.edu>
2018-03-30 21:24:55 -0700

    A Little Bit of Code Cleanup

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"net"
 	"os"
 
-	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 
@@ -12,54 +11,12 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-// cancelAll can cancel all currently running goroutines
-var cancelAll context.CancelFunc
-
-// BoolInt is an integer type alias to create an 'enum' type for responses
-type BoolInt int
-
-// BoolInt constants are used to represent an integer for a specific bool value
-// such that False = 0, and True = 1
-const (
-	FALSE BoolInt = iota
-	TRUE
-)
-
 const (
 	port = ":50051"
 )
 
-// StatusMsg is a type alias to allow for 'enum' style type
-type StatusMsg int
-
-// The constants to represent a Status message
-const (
-	SUCCESS StatusMsg = iota
-	ERROR
-)
-
-// Represents the string representation of the 'status' field in some
-// responses
-var statuses = [...]string{
-	"success",
-	"error",
-}
-
-// IP is this computers IP address
-var IP string
-
+// IP_PORT is this computers IP address
 var IP_PORT string
-
-func init() {
-	log.SetFormatter(&log.TextFormatter{})
-	// Output to stdout instead of the default stderr
-	// Can be any io.Writer, see below for File example
-	log.SetOutput(os.Stdout)
-
-	// Only log the warning severity or above.
-	log.SetLevel(log.InfoLevel)
-
-}
 
 func launchGrpcServer() {
 	lis, err := net.Listen("tcp", port)
