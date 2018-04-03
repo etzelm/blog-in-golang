94902e29636dc103dda65fc2e870c0879c25360d
Mitchell Etzel <metzel@ucsc.edu>
2018-04-03 12:14:48 -0700

    Update

--- run.sh
+++
@@ -1,8 +0,0 @@
-for i in {1..1} ;
-do
-    ( docker stop $(docker ps -aq) );
-    ( docker rm $(docker ps -aq) );
-    ( docker build -t blog . );
-    ( docker run -d -p 8080:3000 blog );
-    ( docker ps -a );
-done
