68a7539f999b462073f30b757a76dac5890b33e5
Mitchell Etzel <metzel@ucsc.edu>
2018-04-12 12:32:34 -0700

    First Paragraph Draft

--- fairy/google_SRE.go
+++ fairy/google_SRE.go
@@ -34,19 +34,26 @@ func google_SRE() {
 	created := "April 11th, 2018"
 	modified := "April 12th, 2018"
 	hold := "<div class=\"well\" style=\"background-color:#DFF0D8;\">" +
-		//Start of the About Me paragraph and contact info
+		//Start of the Introduction paragraph and source code link
 		"<div class=\"panel panel-default\">" +
 		"<div class=\"panel-heading\" style=\"color:#A619FF;font-size: 1.6em\">" +
 		"A Review & Implementation of Google's Site Reliability Engineering:</div>" +
 		"<ul class=\"list-group\">" +
 		"<li class=\"list-group-item\">" +
-		"<h4>&emsp;&emsp;Paying homage to who many consider to be at the forefront of distributed systems, I have deciced " +
-		"to review and implement some of the concepts put forth in the O'Reilly book, <i>" +
+		"<h4>&emsp;&emsp;Paying homage to who many consider to be at the forefront of distributed systems, " +
+		"I have deciced to review and implement some of the concepts put forth in the O'Reilly book, <i>" +
 		"<a style=\"color:#9C6708;\" href=\"https://landing.google.com/sre/book.html\" target=\"_blank\">" +
-		"Site Reliability Engineering: How Google Runs Production Systems</a></i>. This work will be done concurrently " +
-		"with my development of a personal distributed system for graph storage to help foster both projects and I have " +
-		"attached a link to that source code at the bottom of this introduction. " +
-		"" +
+		"Site Reliability Engineering: How Google Runs Production Systems</a></i>. This work will be done " +
+		"concurrently with my development of a distributed system for graph storage, to help foster " +
+		"both projects, and I have attached a link to that source code at the bottom of this introduction. " +
+		"For those readers who are just now hearing about Site Reliability Engineering, " +
+		"SRE is Google's approach to how they develop and manage their internet services like Gmail, " +
+		"Maps, and Drive. When Benjamin Sloss joined Google in 2003 and ran a \"Production Team\" of seven " +
+		"other engineers, he decided to manage the group through software engineering tenants that he had " +
+		"picked up over his years of previous work. Over time, this team and these tenants matured " +
+		"into what is Google's present-day SRE team. One of the first major drives of any Site Reliability " +
+		"Engineer should be to automate as many of their work processes as they can, so as to minimize on the " +
+		"amount of actual \"complex manual labor\" they have to actually perform. " +
 		"</h4></li>" +
 		"<li class=\"list-group-item\" style=\"text-align: center;\">" +
 		"<h4>Source code for this project can be located here:&emsp;&emsp;" +
