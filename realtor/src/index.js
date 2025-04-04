a8a140e363f52b2ae918ec92a3cb6339ceb6e3de
Mitchell Etzel <etzelm@live.com>
2025-04-03 19:30:00 -0700

    update react render

--- realtor/src/index.js
+++ realtor/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-
-ReactDOM.render(
+import { createRoot } from 'react-dom/client';
+const root = createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
