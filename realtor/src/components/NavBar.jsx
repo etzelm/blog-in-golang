c7094bc9d0e1cb054237f8db0f4e4ddd58205e17
Mitchell Etzel <etzelm@live.com>
2025-04-21 13:19:20 -0700

    right align sign in button/menu

--- realtor/src/components/NavBar.jsx
+++ realtor/src/components/NavBar.jsx
@@ -8,28 +8,28 @@ const NavBar = ({ loggedIn, user, onSignIn, onSignOut, authLoading }) => {
         <Navbar.Brand href="/realtor">realtor webpage.</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse>
-          <Nav className="me-auto">
+          <Nav>
             <Nav.Link href="/realtor/search">Search Listings</Nav.Link>
             <Nav.Link href="/realtor/new">List Your Property</Nav.Link>
-            <Nav>
-              {loggedIn ? (
-                <NavDropdown title={user} id="collapsible-nav-dropdown">
-                  <NavDropdown.Item href="/realtor/my-listings">View/Modify Listings</NavDropdown.Item>
-                  <NavDropdown.Item onClick={onSignOut} disabled={authLoading}>
-                    {authLoading ? 'Signing Out...' : 'Sign Out'}
-                  </NavDropdown.Item>
-                </NavDropdown>
-              ) : (
-                <button 
-                  id="loginButton"
-                  onClick={onSignIn}
-                  className="btn btn-light"
-                  disabled={authLoading}
-                >
-                  {authLoading ? 'Signing In...' : 'Sign In'}
-                </button>
-              )}
-            </Nav>
+          </Nav>
+          <Nav className="ml-auto">
+            {loggedIn ? (
+              <NavDropdown title={user} id="collapsible-nav-dropdown">
+                <NavDropdown.Item href="/realtor/my-listings">View/Modify Listings</NavDropdown.Item>
+                <NavDropdown.Item onClick={onSignOut} disabled={authLoading}>
+                  {authLoading ? 'Signing Out...' : 'Sign Out'}
+                </NavDropdown.Item>
+              </NavDropdown>
+            ) : (
+              <button
+                id="loginButton"
+                onClick={onSignIn}
+                className="btn btn-light"
+                disabled={authLoading}
+              >
+                {authLoading ? 'Signing In...' : 'Sign In'}
+              </button>
+            )}
           </Nav>
         </Navbar.Collapse>
       </Container>
